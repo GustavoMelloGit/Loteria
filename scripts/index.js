74398cda98e1553c6a6cc113016b7a63292d68a0
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2021-09-30 09:05:55 -0300

    feat: added function to delete saved games

--- scripts/index.js
+++ scripts/index.js
@@ -6,26 +6,29 @@
         const lotofacil = document.querySelector('[data-js="button-lotofacil"]');
         const megaSena = document.querySelector('[data-js="button-mega-sena"]');
         const lotomania = document.querySelector('[data-js="button-lotomania"]');
-        const gameDescription = document.querySelector('[data-js="gameDescription"]');
+        const gameDescription = document.querySelector(
+            '[data-js="gameDescription"]'
+        );
         const gameName = document.querySelector('[data-js="betGameName"]');
-        const betTable = document.querySelector('#betNumbers');
+        const betTable = document.querySelector("#betNumbers");
         const addToCart = document.querySelector('[data-js="addToCart"]');
+        const gamesInCart = document.querySelector('[data-js="gamesInCart"]');
+        const totalPrice = document.querySelector('[data-js="totalPrice"]');
         let gameSelected;
         let numbersSelected = [];
-        let countNumberSelected;
+        let totalPriceCount = 0;
 
         function verifyStateAjax() {
             if (ajax.readyState === 4 && ajax.status === 200) {
                 response = JSON.parse(ajax.responseText);
                 return true;
-            }
-            else return false;
+            } else return false;
         }
-        async function getDataFromJSON(index) {
+        function getDataFromJSON(index) {
             try {
-                ajax.open('GET', './games.json');
+                ajax.open("GET", "./games.json");
                 ajax.send();
-                ajax.addEventListener('readystatechange', () => {
+                ajax.addEventListener("readystatechange", () => {
                     if (verifyStateAjax()) {
                         gameDescription.textContent = response.types[index].description;
                     }
@@ -36,33 +39,35 @@
         }
 
         function resetButtonsSelected() {
-            lotofacil.style.backgroundColor = 'white';
-            lotofacil.style.color = response.types[0].color;
-            megaSena.style.backgroundColor = "white";
-            megaSena.style.color = response.types[1].color;
-            lotomania.style.backgroundColor = "white";
-            lotomania.style.color = response.types[2].color;
+            if (response) {
+                lotofacil.style.backgroundColor = "white";
+                lotofacil.style.color = response.types[0].color;
+                megaSena.style.backgroundColor = "white";
+                megaSena.style.color = response.types[1].color;
+                lotomania.style.backgroundColor = "white";
+                lotomania.style.color = response.types[2].color;
+            }
         }
 
         function buttonSelected(index) {
             switch (index) {
                 case 0:
                     resetButtonsSelected();
-                    lotofacil.style.backgroundColor = '#7f3992';
+                    lotofacil.style.backgroundColor = "#7f3992";
                     lotofacil.style.color = "white";
-                    gameName.textContent = 'for lotofácil';
+                    gameName.textContent = "for lotofácil";
                     break;
                 case 1:
                     resetButtonsSelected();
                     megaSena.style.backgroundColor = "#01ac66";
                     megaSena.style.color = "white";
-                    gameName.textContent = 'for Mega-sena';
+                    gameName.textContent = "for Mega-sena";
                     break;
                 case 2:
                     resetButtonsSelected();
                     lotomania.style.backgroundColor = "#f79c31";
                     lotomania.style.color = "white";
-                    gameName.textContent = 'for lotomania';
+                    gameName.textContent = "for lotomania";
                     break;
             }
             numbersSelected = [];
@@ -74,16 +79,15 @@
         }
 
         function cleanBetTable() {
-            betTable.innerHTML = '';
+            betTable.innerHTML = "";
         }
         function numberClicked(number) {
             if (numbersSelected.includes(number.value)) {
                 const index = numbersSelected.indexOf(number.value);
 
-                number.style.backgroundColor = '#adc0c4';
+                number.style.backgroundColor = "#adc0c4";
                 numbersSelected.splice(index, 1);
-            }
-            else {
+            } else {
                 switch (gameSelected) {
                     case 0:
                         number.style.backgroundColor = response.types[0].color;
@@ -102,42 +106,128 @@
         function createBetButtons(index) {
             cleanBetTable();
             for (let i = 1; i <= index; i++) {
-                const numbers = document.createElement('button');
-                numbers.classList.add('betNumber');
+                const numbers = document.createElement("button");
+                numbers.classList.add("betNumber");
                 numbers.style.transition = ".2s";
                 numbers.textContent = numberFormat(i);
                 numbers.value = i;
 
-                numbers.addEventListener('click', () => numberClicked(numbers));
+                numbers.addEventListener("click", () => numberClicked(numbers));
 
                 betTable.appendChild(numbers);
             }
         }
 
+        function verifyNumberOfSelected() {
+            if (response.types[gameSelected].max_number < numbersSelected.length || numbersSelected.length === 0) return false;
+            else return true;
+        }
+
+        function returnGameName() {
+            switch (gameSelected) {
+                case 0:
+                    return response.types[0].type;
+                case 1:
+                    return response.types[1].type;
+                case 2:
+                    return response.types[2].type;
+            }
+        }
+
+        function formatPrice(number) {
+            const formated = new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+            })
+            number = formated.format(number);
+            return ` ${number}`;
+        }
+        function calculateGamePrice() {
+            let gamePrice = response.types[gameSelected].price * numbersSelected.length;
+            totalPriceCount += response.types[gameSelected].price * numbersSelected.length;
+            return gamePrice;
+        }
+
+        function calculateTotalPrice() {
+            return `Total: ${formatPrice(totalPriceCount)}`;
+        }
+
+        function handleDeleteGame(deletedGame, price) {
+            deletedGame.remove();
+            totalPriceCount -= price;
+            totalPrice.textContent = formatPrice(totalPriceCount);
+        }
+
+        function handleAddToCart() {
+            const trashCan = document.createElement('img');
+            trashCan.src = '../assets/trash.svg';
+
+            if (verifyNumberOfSelected()) {
+                const game = document.createElement('div');
+                game.classList.add('game');
+
+                const gameInnerContent = document.createElement('div');
+                gameInnerContent.classList.add('gameInnerContent');
+
+                const gameNumbers = document.createElement('span');
+                const wrapper = document.createElement('div');
+
+                const gameName = document.createElement('span');
+                gameName.classList.add('gameName');
+
+                const price = document.createElement('span');
+                const priceNumber = calculateGamePrice();
+
+                price.textContent = formatPrice(priceNumber);
+                gameName.textContent = returnGameName();
+                totalPrice.textContent = calculateTotalPrice();
+
+                wrapper.appendChild(gameName);
+                wrapper.appendChild(price);
+
+                gameNumbers.textContent = numbersSelected;
+                gameInnerContent.appendChild(gameNumbers);
+                gameInnerContent.appendChild(wrapper);
+
+                game.appendChild(trashCan);
+                game.appendChild(gameInnerContent);
+
+                gamesInCart.appendChild(game);
+
+                trashCan.addEventListener('click', () => handleDeleteGame(game, priceNumber));
+            } else {
+                alert(
+                    `Preencha corretamente os campos`
+                );
+            }
+        }
+
         function eventListeners(index) {
-            countNumberSelected++;
             gameSelected = index;
             getDataFromJSON(index);
             buttonSelected(index);
-            createBetButtons(response.types[index].range);
+            if (response) {
+                createBetButtons(response.types[index].range);
+            }
         }
 
-        lotofacil.addEventListener('click', () => {
+        lotofacil.addEventListener("click", () => {
             eventListeners(0);
         });
 
-        megaSena.addEventListener('click', () => {
+        megaSena.addEventListener("click", () => {
             eventListeners(1);
         });
 
-        lotomania.addEventListener('click', () => {
+        lotomania.addEventListener("click", () => {
             eventListeners(2);
         });
-        addToCart.addEventListener('click', () => console.log(numbersSelected))
 
-        document.addEventListener("DOMContentLoaded", () => {
-            lotofacil.click();
+        addToCart.addEventListener("click", () => {
+            handleAddToCart();
         });
+
+        lotofacil.click();
     }
     app();
 })();
